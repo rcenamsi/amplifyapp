3b865e713a1e87cb9934d51a46fe09ef8b5fd4e1
Roms <rom.pxz@gmail.com>
2021-02-09 20:55:36 +0800

    Added Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello from Version 2</h2>
       </header>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
